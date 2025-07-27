c0ef896ae39f7a7f49a2b2205a4b65045163a538
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-07-27 23:22:18 +0200

    Rename PortPlanException to SimPortException

--- src/main/java/nl/tudelft/simulation/simport/util/SimPortException.java
+++ src/main/java/nl/tudelft/simulation/simport/util/SimPortException.java
@@ -1,57 +1,51 @@
 package nl.tudelft.simulation.simport.util;
 
 /**
- * PortPlanException.java.
+ * SimPortException for project-specific exceptions.
  * <p>
  * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class PortPlanException extends Exception
+public class SimPortException extends Exception
 {
+    /** */
+    private static final long serialVersionUID = 1L;
 
     /**
-     * 
+     * Create a SimPortException.
      */
-    public PortPlanException()
+    public SimPortException()
     {
     }
 
     /**
-     * @param message
+     * Create a SimPortException.
+     * @param message the message of the exception
      */
-    public PortPlanException(String message)
+    public SimPortException(final String message)
     {
         super(message);
     }
 
     /**
-     * @param cause
+     * Create a SimPortException.
+     * @param cause underlying exception that caused this exception
      */
-    public PortPlanException(Throwable cause)
+    public SimPortException(final Throwable cause)
     {
         super(cause);
     }
 
     /**
-     * @param message
-     * @param cause
+     * Create a SimPortException.
+     * @param message the message of the exception
+     * @param cause underlying exception that caused this exception
      */
-    public PortPlanException(String message, Throwable cause)
+    public SimPortException(final String message, final Throwable cause)
     {
         super(message, cause);
-    }
-
-    /**
-     * @param message
-     * @param cause
-     * @param enableSuppression
-     * @param writableStackTrace
-     */
-    public PortPlanException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
-    {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
