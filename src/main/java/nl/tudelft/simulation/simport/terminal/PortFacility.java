fd9c4cf6a921092ed72421f09f5abedfdef1266d
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-11-09 15:12:41 +0100

    Create a PortFacility interface for Terminals and Depots

---
+++ src/main/java/nl/tudelft/simulation/simport/terminal/PortFacility.java
@@ -0,0 +1,51 @@
+package nl.tudelft.simulation.simport.terminal;
+
+import org.djutils.base.Identifiable;
+
+import nl.tudelft.simulation.dsol.animation.Locatable;
+import nl.tudelft.simulation.dsol.simulators.clock.ClockDevsSimulatorInterface;
+import nl.tudelft.simulation.simport.model.PortModel;
+
+/**
+ * PortFacility is the interface for deepsea and shortsea terminals, as well as empty depots.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public interface PortFacility extends Identifiable, Locatable
+{
+    /** @return the simulator. */
+    ClockDevsSimulatorInterface getSimulator();
+
+    /** @return the model. */
+    PortModel getModel();
+
+    /**
+     * Set the gate for the terminal.
+     * @param gate the gate for the terminal
+     * @return the terminal for method chaining
+     */
+    DeepseaTerminal setGate(Gate gate);
+
+    /** @return the gate. */
+    Gate getGate();
+
+    /** @return the latitude. */
+    double getLat();
+
+    /** @return the longitude. */
+    double getLon();
+
+    /**
+     * Set the capacity in TEU for the terminal.
+     * @param capacityTeu the capacity in TEU for the terminal
+     * @return the terminal for method chaining
+     */
+    DeepseaTerminal setCapacityTeu(int capacityTeu);
+
+    /** @return the capacity in TEU. */
+    int getCapacityTeu();
+
+}
