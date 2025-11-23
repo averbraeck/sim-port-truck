176ddd86c7a8d5a8b6800394ef947f924d27c16b
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-11-23 16:11:59 +0100

    Introduce Terminal interface for flexible modeling

---
+++ src/main/java/nl/tudelft/simulation/simport/terminal/Terminal.java
@@ -0,0 +1,14 @@
+package nl.tudelft.simulation.simport.terminal;
+
+/**
+ * Terminal.java.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public interface Terminal extends ContainerFacility
+{
+
+}
