2f308a66c4e90420f30d95ae50511cfbed5ee959
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-11-26 12:24:38 +0100

    Add a VesselType (Deep-sea / Short-sea)

---
+++ src/main/java/nl/tudelft/simulation/simport/vessel/VesselType.java
@@ -0,0 +1,18 @@
+package nl.tudelft.simulation.simport.vessel;
+
+/**
+ * VesselType indicates whether a vessel is a deep-sea or a short-sea/feeder vessel.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public enum VesselType
+{
+    /** Deep-sea. */
+    DEEPSEA,
+
+    /** Short-sea or feeder. */
+    SHORTSEA;
+}
