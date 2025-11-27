94b2f3607a0ad8acb836515bc10615df61c39408
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-11-27 11:30:22 +0100

    Method name changes in Terminal

--- src/main/java/nl/tudelft/simulation/simport/terminal/TerminalReaderCsv.java
+++ src/main/java/nl/tudelft/simulation/simport/terminal/TerminalReaderCsv.java
@@ -1,7 +1,6 @@
 package nl.tudelft.simulation.simport.terminal;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.djutils.io.ResourceResolver;
@@ -11,14 +10,14 @@ import de.siegmar.fastcsv.reader.NamedCsvRow;
 import nl.tudelft.simulation.simport.model.PortModel;
 
 /**
- * Read the terminals from a CSV-file.
+ * Read the terminals from a CSV-file. TODO: Clarify 'In' and 'Out'; standardize on import and export seen from the container.
  * <p>
  * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class TerminalCsv
+public class TerminalReaderCsv
 {
     /**
      * Read the terminals from a CSV file.
@@ -32,18 +31,18 @@ public class TerminalCsv
         {
             for (NamedCsvRow row : csvReader)
             {
-                var terminal = new Terminal(row.getField("id"), model, Double.parseDouble(row.getField("lon")),
+                var terminal = new TerminalStandard(row.getField("id"), model, Double.parseDouble(row.getField("lon")),
                         Double.parseDouble(row.getField("lat")));
                 var truckFraction = Double.parseDouble(row.getField("truckIn"));
                 var bargeFraction = Double.parseDouble(row.getField("bargeIn"));
                 var railFraction = Double.parseDouble(row.getField("railIn"));
                 var shortSeaFraction = Double.parseDouble(row.getField("shortSeaIn"));
-                terminal.setModalSplitIn(new ModalSplit(truckFraction, bargeFraction, railFraction, shortSeaFraction));
+                terminal.setModalSplitExport(new ModalSplit(truckFraction, bargeFraction, railFraction, shortSeaFraction));
                 truckFraction = Double.parseDouble(row.getField("truckOut"));
                 bargeFraction = Double.parseDouble(row.getField("bargeOut"));
                 railFraction = Double.parseDouble(row.getField("railOut"));
                 shortSeaFraction = Double.parseDouble(row.getField("shortSeaOut"));
-                terminal.setModalSplitOut(new ModalSplit(truckFraction, bargeFraction, railFraction, shortSeaFraction));
+                terminal.setModalSplitImport(new ModalSplit(truckFraction, bargeFraction, railFraction, shortSeaFraction));
             }
         }
         catch (IOException e)
